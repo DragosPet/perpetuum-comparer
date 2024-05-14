ae59618620a983bb21701c663628474474ee4a1c
DragosPet <iuliandragospet@gmail.com>
2024-05-14 17:41:21 +0300

    WIP. Refactor code and enhance comparison functionality

--- perpetuum_comparer/comparer.py
+++ perpetuum_comparer/comparer.py
@@ -1,28 +1,34 @@
 """Main module for comparing two datasets."""
+
 import logging
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from tabulate import tabulate
+from termcolor import colored
+from utils import logging_setup
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(name)s %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%S%z",
-)
-log = logging.getLogger(__name__)
+log = logging_setup(logging.INFO)
+
 
 class DataComparer:
     """Main comparison class."""
-    def __init__(self, test_name:str, primary_df: pd.DataFrame, secondary_df: pd.DataFrame, line_id: str) -> None:
+
+    def __init__(
+        self,
+        test_name: str,
+        primary_df: pd.DataFrame,
+        secondary_df: pd.DataFrame,
+        line_id: str,
+    ) -> None:
         """Initialize Data Comparer.
-        
+
         Args:
         test_name(str): Test Name used to generate the final reports.
         primary_df(pd.DataFrame): Primary dataframe for comparison.
         secondary_df(pd.DataFrame): Secondary dataframe for comparison.
         line_id(str): Line identifier for dataframes.
-        
+
         Returns:
         None
         """
@@ -30,19 +36,21 @@ class DataComparer:
         self.primary_df = primary_df
         self.secondary_df = secondary_df
         self.line_id = line_id
+        self.exclusive_primary_indexes = []
+        self.exclusive_secondary_indexes = []
 
     def structural_comparison(self) -> bool:
         """Initialize Data Comparer.
-        
+
         Args:
-        
+
         Returns:
         match_flag(bool): Bool flag that will specify if there is any structural difference between the 2 dataframes.
         """
         self.structural_matches = []
         self.structural_diffs = []
         checked_keys = []
-        if self.primary_df.empty :
+        if self.primary_df.empty:
             log.error("Primary DataFrame is empty. Stopping structural comparison!")
             return False
         if self.secondary_df.empty:
@@ -56,93 +64,152 @@ class DataComparer:
         for key in primary_columns.keys():
             if secondary_columns.get(key):
                 if primary_columns[key] == secondary_columns[key]:
-                    self.structural_matches.append(
-                        (key, primary_columns[key])
-                    )
-                else : 
-                    self.structural_diffs.append(
-                        (key, primary_columns[key])
-                    )
+                    self.structural_matches.append((key, primary_columns[key]))
+                else:
+                    self.structural_diffs.append((key, primary_columns[key]))
                     match_flag = False
-            else : 
-                self.structural_diffs.append(
-                    (key, primary_columns[key])
-                )
+            else:
+                self.structural_diffs.append((key, primary_columns[key]))
                 match_flag = False
             checked_keys.append(key)
-        
+
         for key in secondary_columns.keys():
             if key not in checked_keys:
-                self.structural_diffs.append(
-                    (key, secondary_columns[key])
-                )
+                self.structural_diffs.append((key, secondary_columns[key]))
                 match_flag = False
         return match_flag
-    
+
     def display_structural_comparison(self) -> None:
-        print("Structural matches !")
-        print(tabulate(
-            self.structural_matches,
-            headers=["Column", "Pandas Data Type"],
-            tablefmt="grid",
-            showindex="always"
-        ))
-        print("Structural Differences !")
-        print(tabulate(
-            self.structural_diffs,
-            headers=["Column", "Pandas Data Type"],
-            tablefmt="grid",
-            showindex="always"
-        ))
-    
+        print("Structural matches :")
+        print(
+            tabulate(
+                self.structural_matches,
+                headers=["Column", "Pandas Data Type"],
+                tablefmt="grid",
+                showindex="always",
+            )
+        )
+        print("Structural Differences :")
+        print(
+            tabulate(
+                self.structural_diffs,
+                headers=["Column", "Pandas Data Type"],
+                tablefmt="grid",
+                showindex="always",
+            )
+        )
+
     def content_comparison(self) -> None:
         log.info("Comparing data counts")
         primary_count = self.primary_df.shape[0]
         secondary_count = self.secondary_df.shape[0]
-        if primary_count == secondary_count : 
+        if primary_count == secondary_count:
             print(f"Data counts matches between datasets : {primary_count} recs !")
-        else : 
-            print(f"Data counts different between datasets : PRIMARY : {primary_count} VS SECONDARY : {secondary_count} !")
-        if self.structural_matches : 
+        else:
+            print(
+                f"Data counts different between datasets : PRIMARY : {primary_count} VS SECONDARY : {secondary_count} !"
+            )
+        if self.structural_matches:
             filter_cols = [x[0] for x in self.structural_matches]
             print(filter_cols)
             subset_primary_df = self.primary_df[filter_cols]
             subset_secondary_df = self.secondary_df[filter_cols]
-        
+
         differences = []
-        for index, rec in subset_primary_df.iterrows() :
-            index_dict = {
-                "index": index,
-                "key_differences": []
-            }
-            compared_secondary_df = subset_secondary_df[subset_secondary_df[self.line_id] == subset_primary_df[self.line_id]]
-            compared_secondary_dict = compared_secondary_df.to_dict(orient="records")[index]
-            for key in filter_cols:
-                if rec[key] != compared_secondary_dict[key]:
-                    print(rec[key], compared_secondary_dict[key])
-                    index_dict["key_differences"].append(key)
-                    differences.append(index_dict)
-        print(differences)
-        return differences 
+        for index, rec in subset_primary_df.iterrows():
+            index_dict = {"index": index, "key_differences": [], "secondary_val": []}
+            if rec[self.line_id] in subset_secondary_df[self.line_id].to_list():
+                compared_secondary_df = subset_secondary_df[
+                    subset_secondary_df[self.line_id] == rec[self.line_id]
+                ]
+                compared_secondary_dict = compared_secondary_df.to_dict(
+                    orient="records"
+                )[0]
+                for key in filter_cols:
+                    if rec[key] != compared_secondary_dict[key]:
+                        print(rec[key], compared_secondary_dict[key])
+                        index_dict["key_differences"].append(key)
+                        index_dict["secondary_val"].append(compared_secondary_dict[key])
+                        differences.append(index_dict)
+            else:
+                self.exclusive_primary_indexes.append(index)
+
+        # get secondary df exclusive lines
+        for ind, x in subset_secondary_df.iterrows():
+            if x[self.line_id] not in subset_primary_df[self.line_id].to_list():
+                self.exclusive_secondary_indexes.append(ind)
+        return differences
+
+    def generate_reports(self, differences_array: dict) -> "tuple[pd.DataFrame]":
+        reports = []
+        if len(differences_array) > 0:
+            for entry in differences_array:
+                report_line = self.primary_df.iloc[[entry["index"]]].to_dict(
+                    orient="records"
+                )[0]
+                for diff in entry["key_differences"]:
+                    report_line[diff] = (
+                        f"{report_line[diff]}/{colored(entry['secondary_val'][0],'red')}"
+                    )
+                reports.append(report_line)
+
+        diff_reports = pd.DataFrame(reports, columns=self.primary_df.columns)
+        primary_exclusive_reports = pd.DataFrame(
+            self.primary_df.iloc[self.exclusive_primary_indexes]
+        )
+        secondary_exclusive_reports = pd.DataFrame(
+            self.secondary_df.iloc[self.exclusive_secondary_indexes]
+        )
+        return (diff_reports, primary_exclusive_reports, secondary_exclusive_reports)
 
 
 if __name__ == "__main__":
-    df1 = pd.DataFrame({
-        "col1": [1,2,3],
-        "col2": [3,5,6],
-        "col3": [8,1,3]
-    },columns=["col1","col2","col3"])
+    df1 = pd.DataFrame(
+        {"col1": [1, 2, 3, 4], "col2": [3, 5, 6, 8], "col3": [8, 1, 3, 7]},
+        columns=["col1", "col2", "col3"],
+    )
 
-    df2 = pd.DataFrame({
-        "col1": [1,2,3],
-        "col2": [3,7,6],
-        "col3": [6,1,3],
-    },columns=["col1","col2","col3"])
+    df2 = pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 5],
+            "col2": [3, 7, 6, 8],
+            "col3": [6, 1, 3, 2],
+            "col4": [7, 2, 4, 0],
+        },
+        columns=["col1", "col2", "col3", "col4"],
+    )
 
     print(df1)
     print(df2)
 
-    dc = DataComparer("test", df1, df2,"col1")
+    dc = DataComparer("test", df1, df2, "col1")
     matching_flag = dc.structural_comparison()
-    dc.content_comparison()
+    diffs = dc.content_comparison()
+    print(dc.exclusive_primary_indexes, dc.exclusive_secondary_indexes)
     dc.display_structural_comparison()
+    (common_diffs, primary_exclusive, secondary_exclusive) = dc.generate_reports(diffs)
+
+    print(
+        tabulate(
+            common_diffs,
+            headers=common_diffs.columns,
+            tablefmt="grid",
+            showindex="always",
+        )
+    )
+    print(
+        tabulate(
+            primary_exclusive,
+            headers=primary_exclusive.columns,
+            tablefmt="grid",
+            showindex="always",
+        )
+    )
+    print(
+        tabulate(
+            secondary_exclusive,
+            headers=secondary_exclusive.columns,
+            tablefmt="grid",
+            showindex="always",
+        )
+    )
---
+++ perpetuum_comparer/utils.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import pandas as pd
+
+
+def logging_setup(log_level: int) -> logging.Logger:
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+    )
+    log = logging.getLogger(__name__)
+    return log
+
+
+def test_valid_path(input_path: str) -> bool:
+    """Given input path, check if it exists."""
+    valid_flag = False
+    if os.path.exists(input_path):
+        valid_flag = True
+    return valid_flag
+
+
+def read_df_from_path(
+    input_path: str, log: logging.Logger, input_format: str = "csv"
+) -> pd.DataFrame:
+    """Given input file path, read file content and return data Pandas DataFrame."""
+    output_data = pd.DataFrame()
+    if test_valid_path(input_path):
+        log.info(f"Path {input_path} available. Reading data file.")
+        if input_path == "csv":
+            output_data = pd.read_csv(input_path)
+        else:
+            log.error(
+                "Unable to read data. Invalid format provided. Returning empty DataFrame."
+            )
+    else:
+        log.error(
+            "Unable to read data. Invalid path provided. Returning empty DataFrame."
+        )
+    return output_data
